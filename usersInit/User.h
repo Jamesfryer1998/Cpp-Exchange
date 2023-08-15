9d9559cb7757afffb1a35b12e9b7963eb6cda6aa
james fryer <jamesfryer1998@outlook.com>
2023-08-15 23:06:04 +0100

    Adding Users

---
+++ usersInit/User.h
@@ -0,0 +1,18 @@
+
+
+class User{
+    public:
+
+        // To generate USER_ID
+        User(std::string firstName,
+             std::string lastName,
+             std::string email,
+        );
+        
+
+        void init();
+
+
+    private:
+
+};
